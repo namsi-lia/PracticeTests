4e544c6755a67d188fb9945302db6735b90cd88f
namsi-elikunda <namsi.elikunda@turnkeyafrica.com>
2022-05-23 19:36:18 +0300

    updatePalindromes

--- updatePalindrome.java
+++ updatePalindrome.java
@@ -6,16 +6,15 @@ eg motor contains oto which is a palindrome
 import java.util.Scanner;
 
 public class updatePalindrome {
-
+    static boolean checkValidity = false;
     public static void main(String[] args) {
         String baseWord = "";
-        boolean checkValidity = true;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a String/Number to check if its a palindrome");
         baseWord = scanner.nextLine();
-        for (int l = 0; l > 0; l++) {
+        for (int l = -1; l>0; l++) {
             if (baseWord.charAt(l) != baseWord.charAt(0)) {
-                checkValidity = false;
+                checkValidity = true;
                 break;
             }
         }
