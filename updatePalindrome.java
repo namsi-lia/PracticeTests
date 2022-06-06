d93a867d45aa7be4774a35be4623aac76ca42263
namsi-elikunda <namsi.elikunda@turnkeyafrica.com>
2022-06-06 09:33:45 +0300

    update palindrome function

--- updatePalindrome.java
+++ updatePalindrome.java
@@ -6,25 +6,38 @@ eg motor contains oto which is a palindrome
 import java.util.Scanner;
 
 public class updatePalindrome {
-    static boolean checkValidity = false;
-    public static void main(String[] args) {
+    public static boolean checkValidity (String string,int baseWord,int reverse) {
+       if (baseWord == reverse) {
+           return true;
+       }
+       if (string.charAt(baseWord) != string.charAt(reverse)) {
+           return false;
+       }
+       if (baseWord < reverse + 1) {
+           return checkValidity(string, baseWord + 1, reverse - 1);
+       }
+        return true;
+    }
+    static boolean checkPalindrome(String string){
+        int j=string.length();
+        if(j==0){
+          return true;
+        }
+        return checkValidity(string,0,j-1);
+    }
+    public static void main(String[]args){
         String baseWord = "";
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a String/Number to check if its a palindrome");
         baseWord = scanner.nextLine();
-        for (int l = -1; l>0; l++) {
-            if (baseWord.charAt(l) != baseWord.charAt(0)) {
-                checkValidity = true;
-                break;
-            }
-        }
-        if (checkValidity) {
+        if(checkPalindrome(baseWord)){
             System.out.println("The entered String/Number is a palindrome");
-
-        } else {
+        }else{
             System.out.println("The entered String/Number is not a palindrome");
         }
 
-
     }
 }
+
+
+
